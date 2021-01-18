309cd3fb4fb8565f956e809b258bb5407fb40c0c
zjma <meshiadia@gmail.com>
2021-01-18 02:52:31 -0800

    new bot api

---
+++ BotApi/main.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from fastapi import Request, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/")
+async def update(request: Request):
+    body = await request.body()
+    print(body)
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
