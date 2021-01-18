f66a94eaa5ee2e4b4e0989e2002457e16bf6623b
zjma <meshiadia@gmail.com>
2021-01-18 03:41:18 -0800

    apply servo signal

--- BotApi/main.py
+++ BotApi/main.py
@@ -1,9 +1,32 @@
-from typing import Optional
-
 from fastapi import Request, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import json
+from picar import new_driving_wheels as dw
+from picar import Servo
+from threading import Thread
+import time
 
 app = FastAPI()
+
+DesiredState = {
+    'PanSpeed': 0,
+    'TiltSpeed': 0,
+}
+
+def applier():
+    tiltServo = Servo.Servo(1)
+    panServo = Servo.Servo(2)
+    currentPanDegree = 90
+    currentTiltDegree = 90
+
+    while True:
+        currentPanDegree = ensureGoodAngle(currentPanDegree+DesiredState['PanSpeed']*60)
+        currentTiltDegree = ensureGoodAngle(currentTiltDegree+DesiredState['TiltSpeed']*60)
+        tiltServo.write(currentTiltDegree)
+        panServo.write(currentPanDegree)
+        time.sleep(0.1)
+
+T = Thread(target=applier).start()
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,11 +37,7 @@ app.add_middleware(
 )
 
 @app.post("/")
-async def update(request: Request):
+async def newState(request: Request):
+    global DesiredState
     body = await request.body()
-    print(body)
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
+    DesiredState = json.loads(body)
