f1f3107c9a3a9db6b9f1c571818db07ae48ab05b
zjma <meshiadia@gmail.com>
2021-01-18 23:41:05 -0800

    circuit breaker at bot side

--- BotApi/main.py
+++ BotApi/main.py
@@ -1,56 +1,107 @@
-from fastapi import Request, FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, jsonify, request
 import json
+import picar
 from picar import new_driving_wheels as dw
 from picar import Servo
 from threading import Thread
 import time
 
-app = FastAPI()
+import signal
+import sys
 
-DesiredState = {
-    'PanSpeed': 0,
-    'TiltSpeed': 0,
-    'LeftWheelSpeed': 0,
-    'RightWheelSpeed': 0,
-}
+picar.setup()
 
-def ensureGoodAngle(x):
-    x=int(x)
-    x=min(180,x)
-    x=max(0,x)
-    return x
+ShuttingDown = False
 
-def applier():
+def signal_handler(sig, frame):
+    global ShuttingDown
+    ShuttingDown = True
+    print('Shutting down.')
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+class MunyaCar:
+    def __init__(self):
+        self.TargetLeftWheelVelocity = 0
+        self.TargetRightWheelVelocity = 0
+        self.CurrentLeftWheelVelocity = 0
+        self.CurrentRightWheelVelocity = 0
+        self.PanVelocity = 0
+        self.TiltVelocity = 0
+        self.CurrentTiltAngle = 90
+        self.CurrentPanAngle = 90
+        self.LastTargetUpdateTime = 0.0
+bot = MunyaCar()
+
+def apiMain():
+    app = Flask(__name__)
+    @app.route('/', methods=['POST'])
+    def newTarget():
+        bot.TargetLeftWheelVelocity = request.json['TargetLeftWheelVelocity']
+        bot.TargetRightWheelVelocity = request.json['TargetRightWheelVelocity']
+        bot.PanVelocity = request.json['PanVelocity']
+        bot.TiltVelocity = request.json['TiltVelocity']
+        bot.LastTargetUpdateTime = time.time()
+        return jsonify({'Accepted':True})
+    app.run(host='0.0.0.0', port=8002, debug=False, use_reloader=False)
+
+def motorOperationMain():
     tiltServo = Servo.Servo(1)
     panServo = Servo.Servo(2)
-    currentPanDegree = 90
-    currentTiltDegree = 90
-
-    while True:
-        currentPanDegree = ensureGoodAngle(currentPanDegree+DesiredState['PanSpeed']*6)
-        currentTiltDegree = ensureGoodAngle(currentTiltDegree+DesiredState['TiltSpeed']*6)
-        tiltServo.write(currentTiltDegree)
-        panServo.write(currentPanDegree)
-        dw.setSpeed(int(DesiredState['LeftWheelSpeed']*100),int(DesiredState['RightWheelSpeed']*100))
+    while not ShuttingDown:
+        dw.setSpeed(int(bot.CurrentLeftWheelVelocity*100),int(bot.CurrentRightWheelVelocity*100))
+        tiltServo.write(bot.CurrentTiltAngle)
+        panServo.write(bot.CurrentPanAngle)
         time.sleep(0.01)
 
-T = Thread(target=applier).start()
+def currentUpdaterMain():
+    # Parameters.
+    DesiredMotorZeroToOneTimeInSeconds = 2.0
+    DesiredServoTurnaroundTimeInSeconds = 0.5
+    DesiredRefreshFrequency = 100
+    
+    # Derived.
+    VelocityDiffMax = 1.0 / (DesiredRefreshFrequency * DesiredMotorZeroToOneTimeInSeconds)
+    AngleDiffMax = 180.0 / (DesiredRefreshFrequency * DesiredServoTurnaroundTimeInSeconds)
+    
+    def getNextVelocity(current, target):
+        diff = target-current
+        diff = min(diff, VelocityDiffMax)
+        diff = max(diff, -VelocityDiffMax)
+        return current+diff
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    def getNextAngle(current, velocity):
+        next = current + velocity*6
+        next = min(next, 180)
+        next = max(next, 0)
+        return next
 
-@app.put("/")
-async def setState(request: Request):
-    global DesiredState
-    body = await request.body()
-    DesiredState = json.loads(body)
+    while not ShuttingDown:
+        bot.CurrentLeftWheelVelocity = getNextVelocity(bot.CurrentLeftWheelVelocity, bot.TargetLeftWheelVelocity)
+        bot.CurrentRightWheelVelocity = getNextVelocity(bot.CurrentRightWheelVelocity, bot.TargetRightWheelVelocity)
+        bot.CurrentTiltAngle = getNextAngle(bot.CurrentTiltAngle, bot.TiltVelocity)
+        bot.CurrentPanAngle = getNextAngle(bot.CurrentPanAngle, bot.PanVelocity)
+        time.sleep(1/DesiredRefreshFrequency)
 
-@app.get("/")
-def getState():
-    return DesiredState
+def targetUpdaterMain():
+    while not ShuttingDown:
+        if bot.LastTargetUpdateTime < time.time()-0.1:
+            bot.TargetLeftWheelVelocity = 0
+            bot.TargetRightWheelVelocity = 0
+        time.sleep(0.01)
+
+if __name__=='__main__':
+    apiThread = Thread(target=apiMain, daemon=True)
+    motorOperationThread = Thread(target=motorOperationMain)
+    currentUpdaterThread = Thread(target=currentUpdaterMain)
+    targetUpdaterThread = Thread(target=targetUpdaterMain)
+    apiThread.start()
+    motorOperationThread.start()
+    currentUpdaterThread.start()
+    targetUpdaterThread.start()
+    motorOperationThread.join()
+    currentUpdaterThread.join()
+    targetUpdaterThread.join()
+
+
